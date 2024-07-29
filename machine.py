92d2df4b5019507ac241a25bc69196c51f29aaf9
Carter Sperry <sperrycarter@gmail.com>
2024-07-29 16:42:08 -0600

    updates

--- machine.py
+++ machine.py
@@ -1,3 +1,11 @@
+from PIL import Image, ImageDraw, ImageFont
+
 class Machine:
-    def __init__(self) -> None:
+    def __init__(self, rows, probability) -> None:
+        self._rows = rows
+        self._probability = probability
+    
+
+    def display_slot_machine(self):
+        #display the machine with characters
         pass
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from machine import Machine
 from player import Player
 
 def main():
-    player = Player("1", "Carter Sperry")
-    Player.get_player_info(player)
+    player1 = Player("1", "Carter Sperry")
+    Player.get_player_info(player1)
+    machine1 = Machine("hi", "hi")
+    machine1.display_slot_machine()
 
 if __name__ == "__main__":
     main()
